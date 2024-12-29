3dc849f0c1af4f972a96056fc52cdd0ac71e1b3e
rima <uni.rima.rayya@gmail.com>
2024-12-29 14:40:47 +0300

    feat: add order readings page component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,13 @@
-import Linechart from './components/Linechart';
 import './main.css';
-import Piechart from './components/Piechart';
-import Form from './styles/Form';
-import { useReadingsStore } from './stores/readings.store';
-import Cards from './styles/Cards';
 import { ThemeProvider } from './components/ThemeProvider';
-import Barchart from './components/Barchart';
+import OrderReadings from './pages/OrderReadings';
 
-const App = () => {
-	useReadingsStore.getState().setDomain(0, null);
+export default function App() {
 	return (
 		<>
 			<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-				<Form />
-				<div className="flex items-start">
-					<div className="w-[65%]">
-						<Cards />
-						<div className="ml-2">
-							<Linechart />
-						</div>
-					</div>
-
-					<div className="w-[45%]">
-						<div className="ml-2">
-							<Barchart />
-						</div>
-						<div className="ml-2">
-							<Piechart />
-						</div>
-					</div>
-				</div>
+				<OrderReadings />
 			</ThemeProvider>
 		</>
 	);
-};
-
-export default App;
+}
--- src/stores/readings.store.ts
+++ src/stores/readings.store.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import allReadings from '../data/full-trip-140-km.json';
+import allReadings from '../data/messed-up-map-readings.json';
 import { IReading } from '../types/reading.type';
 
 interface ReadingsStore {
