1835cc1d9d289eaa35ec68ff92a5b51c01a02c36
rima <uni.rima.rayya@gmail.com>
2024-12-29 05:28:30 +0300

    feat: add dark mode and customized theme.

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,27 @@ import Piechart from './components/Piechart';
 import Form from './styles/Form';
 import { useReadingsStore } from './stores/readings.store';
 import Cards from './styles/Cards';
+import { ThemeProvider } from './components/ThemeProvider';
 
 const App = () => {
 	useReadingsStore.getState().setDomain(0, null);
 	return (
 		<>
-			<Form />
-			<div className="flex items-start">
-				<div className="w-[70%]">
-					<Cards />
-					<div className="ml-4">
-						<Linechart />
+			<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+				<Form />
+				<div className="flex items-start">
+					<div className="w-[70%]">
+						<Cards />
+						<div className="ml-4">
+							<Linechart />
+						</div>
+					</div>
+
+					<div className="w-[45%]">
+						<Piechart />
 					</div>
 				</div>
-
-				<div className="w-[45%]">
-					<Piechart />
-				</div>
-			</div>
+			</ThemeProvider>
 		</>
 	);
 };
